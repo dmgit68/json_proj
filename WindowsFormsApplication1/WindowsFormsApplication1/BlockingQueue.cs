880a6afdd61f2a9f8e0a460fd6e0446a65e71088
dm@dm.com <dm@dm.com>
2014-10-29 18:12:14 +0100

    add blocking queue proj

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/BlockingQueue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+
+
+    public class BlockingQueue<T>
+    {
+        internal class Node
+        {
+            internal T _item;
+            internal Node _next;
+
+            public Node() { }
+            public Node(T item, Node next) { _item = item; _next = next; }
+        }
+
+        private Object _lockObj;
+        private Node _tail;
+        private Node _head;
+
+        public BlockingQueue()
+        {
+            _lockObj = new Object();
+            _head = _tail = new Node(default(T), null);
+        }
+
+        public void Enqueue(T item)
+        {
+            Node newNode = new Node(item, null);
+
+            lock (_lockObj)
+            {
+                _tail._next = newNode;
+                _tail = newNode;
+
+                Monitor.Pulse(_lockObj);
+            }
+        }
+
+        public T Dequeue()
+        {
+            T retItem;
+
+            lock (_lockObj)
+            {
+                while (_head._next == null)
+                    Monitor.Wait(_lockObj);
+
+                retItem = _head._next._item;
+                _head = _head._next;
+
+                return retItem;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        public class Worker
+        {
+            private BlockingQueue<String> m_queue;
+
+            public Worker(BlockingQueue<String> aQueue)
+            {
+                m_queue = aQueue; 
+            }
+
+            // This method will be called when the thread is started. 
+            public void DoWork(String aStr)
+            {
+                while (!_shouldStop)
+                {
+                    Console.WriteLine("worker thread: waiting...");
+                    object obj = m_queue.Dequeue();
+
+                    String myStr = obj as String;
+                    Console.WriteLine(myStr);
+                    
+                }
+                Console.WriteLine("worker thread: terminating gracefully.");
+            }
+            public void RequestStop()
+            {
+                _shouldStop = true;
+            }
+            // Volatile is used as hint to the compiler that this data 
+            // member will be accessed by multiple threads. 
+            private volatile bool _shouldStop;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            BlockingQueue<String> queue = new BlockingQueue<String>();
+
+            Worker workerObject = new Worker(queue);
+            //Thread workerThread = new Thread(workerObject.DoWork());
+           // new ThreadStart(workerObject.DoWork("hello")) => download(filename));
+            Thread workerThread = new Thread(() => workerObject.DoWork("hello"));
+            // Start the worker thread.
+            workerThread.Start();
+            Console.WriteLine("main thread: Starting worker thread...");
+
+            // Loop until worker thread activates. 
+            while (!workerThread.IsAlive);
+
+
+            String myStr = "1";
+
+            queue.Enqueue(myStr);
+
+           // Thread.Sleep(1000);
+
+            queue.Enqueue("2");
+
+
+            //Thread.Sleep(10000);
+            workerObject.RequestStop();
+
+           // queue.Enqueue(10);
+            workerThread.Join();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextWriter tw = File.CreateText(@"output.txt");
+            tw.WriteLine("hello");
+            tw.Flush();
+            tw.WriteLine("world");
+           // tw.Close();
+        }
+    }
+}
